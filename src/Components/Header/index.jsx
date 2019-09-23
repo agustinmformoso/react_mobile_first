cb391ace41005eaf557838a4f692e19439469b52
Agustin Formoso <agustinf@lagash.com>
2019-09-23 14:32:51 -0300

    transition added

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -72,6 +72,7 @@ const Header = ({ amount, interval, roundedCircle, width, profile_picture }) => 
     const [index, setIndex] = useState()
     const [hideElement, setHideElement] = useState(false)
     const [customClass, setCustomClass] = useState('')
+    const [show, setShow] = useState('')
 
     const handleIndex = (selectedIndex) => {
         switch (selectedIndex) {
@@ -95,8 +96,10 @@ const Header = ({ amount, interval, roundedCircle, width, profile_picture }) => 
     const handleHideElement = (hideElement) => {
         if (hideElement === true) {
             setCustomClass('hideElement')
+            setShow('d-none')
         } else {
-            setCustomClass('')
+            setCustomClass('showElement')
+            setShow(' ')
         }
 
         setHideElement(!hideElement)
@@ -105,13 +108,13 @@ const Header = ({ amount, interval, roundedCircle, width, profile_picture }) => 
 
 
     return (
-        <Col xs={12} className="header text-center pt-4 pb-4" interval={interval} >
+        <Col xs={12} className={`carousel header text-center pt-4 pb-4 ` + customClass} interval={interval} >
             <Image src={profile_picture} roundedCircle={roundedCircle} width={width} />
-            <div className={`p-0 m-0 ` + customClass}>
+            <div className={`p-0 m-0 carousel ` + show}>
                 <h2 className="mt-4 mb-2">{saldo.monedaInicial} {saldo.saldoInicial}</h2>
             </div>
             <a href="#pepe" className="d-block my-4 hide-balance" onClick={() => handleHideElement(hideElement)}>{GLOBAL.HEADER.HIDE_BALANCE}</a>
-            <div className={`p-0 m-0 ` + customClass}>
+            <div className={`p-0 m-0 carousel ` + show}>
                 <Carousel className="pb-4" controls={false} activeIndex={index} onSelect={handleIndex}>
                     <Carousel.Item>
                         <h3 className="mb-5 mt-3">{GLOBAL.HEADER.CURRENCY1}</h3>
